46215762f0157122e369cb6584245106b6a5f3e3
jaysilver-5 <udojoshua3@gmail.com>
2023-12-09 16:59:40 +0100

    ci: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import {
   metamaskWallet,
   coinbaseWallet,
   walletConnect,
-  useAddress,
 } from '@thirdweb-dev/react';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -19,6 +18,7 @@ export const metadata = {
 
 import { ConnectWallet, lightTheme } from '@thirdweb-dev/react';
 import Image from 'next/image';
+
 export default function RootLayout({
   children,
 }: {
@@ -35,6 +35,15 @@ export default function RootLayout({
       ]}
     >
       <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content={metadata.description} />
+          <title>{metadata.title}</title>
+
+          {/* Add other meta tags, link tags, or stylesheets as needed */}
+        </head>
+
         <body
           className={`bg-gradient bg-cover py-2 bg-no-repeat w-full min-h-screen flex justify-between h-screen flex-col items-center ${inter.className}`}
         >
